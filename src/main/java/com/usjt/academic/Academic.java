2319a28a24990fc18a4d17807b43020517eb73e2
asbonato <asbonato@gmail.com>
2024-04-14 01:47:26 -0300

    Versão inicial.

---
+++ src/main/java/com/usjt/academic/Academic.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.usjt.academic;
+
+/**
+ *
+ * @author asbonato
+ */
+public class Academic {
+
+    public static void main(String[] args) {
+        LoginTela tela = new LoginTela();
+        tela.setVisible(true);
+    }
+}
